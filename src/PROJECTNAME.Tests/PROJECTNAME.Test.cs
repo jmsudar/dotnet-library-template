686f4459645c1d18cc9ec54a6a8434985af6736b
jmsudar <code.jmsudar@gmail.com>
2024-05-19 12:54:30 -0700

    Update PROJECTNAME.Test.cs

--- src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
+++ src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
@@ -1,1 +1,9 @@
+//Disable at the beginning of testing
+#pragma warning disable CS8600 // Suppressing CS8600
+#pragma warning disable CS8602 // Suppressing CS8602
+#pragma warning disable CS8629 // Suppressing CS8629
 
+//Re-enable at the end of testing
+#pragma warning restore CS8600 // Suppressing CS8600
+#pragma warning restore CS8602 // Suppressing CS8602
+#pragma warning restore CS8629 // Suppressing CS8629
