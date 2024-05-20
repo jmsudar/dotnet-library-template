5418058165000dab423af6aaa7c2ac41af2014a0
jmsudar <code.jmsudar@gmail.com>
2024-05-20 16:58:12 -0700

    Update PROJECTNAME.Test.cs

--- src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
+++ src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
@@ -1,9 +1,24 @@
-//Disable at the beginning of testing
-#pragma warning disable CS8600 // Suppressing CS8600
-#pragma warning disable CS8602 // Suppressing CS8602
-#pragma warning disable CS8629 // Suppressing CS8629
+/// <summary>
+/// Placeholder summary
+/// </summary>
+[TestClass]
+public class JsonMethodsTests
+{
 
-//Re-enable at the end of testing
-#pragma warning restore CS8600 // Suppressing CS8600
-#pragma warning restore CS8602 // Suppressing CS8602
-#pragma warning restore CS8629 // Suppressing CS8629
+  //Disable at the beginning of testing
+  #pragma warning disable CS8600 // Suppressing CS8600
+  #pragma warning disable CS8602 // Suppressing CS8602
+  #pragma warning disable CS8629 // Suppressing CS8629
+
+  /// <summary>
+  /// Placeholder summary
+  /// </summary>
+  [TestMethod]
+  public void MethodUnderTest_Case_Expectation()
+  { }
+
+  //Re-enable at the end of testing
+  #pragma warning restore CS8600 // Suppressing CS8600
+  #pragma warning restore CS8602 // Suppressing CS8602
+  #pragma warning restore CS8629 // Suppressing CS8629
+}
