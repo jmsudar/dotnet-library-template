414734185eff75ccd22fd3ebce4cc3ff4d6d25d6
jmsudar <code.jmsudar@gmail.com>
2024-05-20 17:34:11 -0700

    Update PROJECTNAME.Test.cs

--- src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
+++ src/PROJECTNAME.Tests/PROJECTNAME.Test.cs
@@ -5,20 +5,20 @@
 public class MethodsTest
 {
 
-  //Disable at the beginning of testing
-  #pragma warning disable CS8600 // Suppressing CS8600
-  #pragma warning disable CS8602 // Suppressing CS8602
-  #pragma warning disable CS8629 // Suppressing CS8629
+    //Disable at the beginning of testing
+    #pragma warning disable CS8600 // Suppressing CS8600
+    #pragma warning disable CS8602 // Suppressing CS8602
+    #pragma warning disable CS8629 // Suppressing CS8629
 
-  /// <summary>
-  /// Placeholder summary
-  /// </summary>
-  [TestMethod]
-  public void MethodUnderTest_Case_Expectation()
-  { }
+    /// <summary>
+    /// Placeholder summary
+    /// </summary>
+    [TestMethod]
+    public void MethodUnderTest_Case_Expectation()
+    { }
 
-  //Re-enable at the end of testing
-  #pragma warning restore CS8600 // Suppressing CS8600
-  #pragma warning restore CS8602 // Suppressing CS8602
-  #pragma warning restore CS8629 // Suppressing CS8629
+    //Re-enable at the end of testing
+    #pragma warning restore CS8600 // Suppressing CS8600
+    #pragma warning restore CS8602 // Suppressing CS8602
+    #pragma warning restore CS8629 // Suppressing CS8629
 }
