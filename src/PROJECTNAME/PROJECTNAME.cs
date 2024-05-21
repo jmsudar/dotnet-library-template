838111dbf9ad49cca00faef1ab4ff92a57175f7e
jmsudar <code.jmsudar@gmail.com>
2024-05-20 17:33:21 -0700

    Update PROJECTNAME.cs

--- src/PROJECTNAME/PROJECTNAME.cs
+++ src/PROJECTNAME/PROJECTNAME.cs
@@ -1,18 +1,16 @@
-namespace REPONAME
+namespace jmsudar.DotNet.Test
 {
-  /// <summary>
-  /// Summary placeholder
-  /// </summary>  
-  public static class Methods
-  {
     /// <summary>
     /// Summary placeholder
-    /// </summary>
-    /// <param name="example">Example parameter</param>
-    /// <returns>Example return type</returns>
-    public static void Method()
+    /// </summary>  
+    public static class Methods
     {
-      
+        /// <summary>
+        /// Summary placeholder
+        /// </summary>
+        /// <param name="example">Example parameter</param>
+        /// <returns>Example return type</returns>
+        public static void Method()
+        { }
     }
-  }
 }
